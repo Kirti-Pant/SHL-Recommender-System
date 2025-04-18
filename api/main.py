a532d96f9fd41543af385420432cf5f58177a644
Kirti Pant <dakshitapant19@gmail.com>
2025-04-18 18:16:09 +0530

    Add FastAPI backend with /recommend endpoint

---
+++ api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utils.recommender import recommend_assessments
+from utils.query_enhancer import enhance_query
+
+app = FastAPI()
+
+class QueryInput(BaseModel):
+    query: str
+
+@app.post("/recommend")
+async def recommend(input_data: QueryInput):
+    enhanced_query = enhance_query(input_data.query)
+    results = recommend_assessments(enhanced_query)
+    return results
